dc2f2d3bcd281ec97fc665c6e545f61b1cc96e83
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-22 19:10:14 -0300

    feat:  seen as running a program in python, data types and variables and constants

---
+++ tipos-de-dados.py
@@ -0,0 +1,6 @@
+print("tipo inteiro (int): ", 11 + 10)
+print("tipo ponto flutuante (float): ", 1.5 + 2.5)
+print("tipo booleano (bool) true: ", True)
+print("tipo booleano (bool) false: ", False)
+print("tipo string (str): ", "Olá Mundo!")
+
---
+++ variaveis-constantes.py
@@ -0,0 +1,12 @@
+nome = 'Danner' # Declaração de variável string
+idade = 23 # Declaração de variável inteiro
+
+nome, idade = 'Jhon', 30 # Reatribuição de variável
+
+print(nome, idade) # Impressão de variáveis
+
+limite_saque_diario = 500.00 # Declaração de uma variável com snake_case
+print(limite_saque_diario)
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'MG', 'ES'] # Declaração de uma constante com letras maiúsculas
+print(BRAZILIAN_STATES)
