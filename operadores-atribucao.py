f5355c92a1b06103d274e4ce32573693deda0321
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-23 20:10:12 -0300

    feat: add operadores de atribuição

---
+++ operadores-atribucao.py
@@ -0,0 +1,19 @@
+saldo = 20
+print(f'o valor do saldo é {saldo}')
+saldo = 400
+print(f'o valor do saldo sobrescreve o saldo é {saldo} ')
+saldo += 100  # saldo = saldo + 100
+print(f'o valor do saldo com acréscimo de 100 é {saldo}')
+saldo -= 50    # saldo = saldo - 50
+print(f'o valor do saldo com desconto de 50 é {saldo}')
+saldo *= 2    # saldo = saldo * 2
+print(f'o valor do saldo dobrado é {saldo}')
+saldo /= 4     # saldo = saldo / 4
+print(f'o valor do saldo dividido por 4 é {saldo}')
+saldo %= 3    # saldo = saldo % 3
+print(f'o valor do saldo módulo 3 é {saldo}')
+saldo **= 2   # saldo = saldo ** 2
+print(f'o valor do saldo elevado ao quadrado é {saldo}')
+saldo //= 2  # saldo = saldo // 2
+print(f'o valor do saldo dividido por 2 é {saldo}')
+
