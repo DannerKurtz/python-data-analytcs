fd36bb3183100c3bae238f99b3ad24230601b79f
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-23 19:52:49 -0300

    feat: add operadores de comparação

---
+++ operadores-de-comparacao.py
@@ -0,0 +1,11 @@
+saldo = 200
+saque = 200
+
+print(f'saldo é maior que saque? {saldo > saque}')
+print(f'saldo é maior ou igual a saque? {saldo >= saque}')
+print(f'saldo é menor que saque? {saldo < saque}')
+print(f'saldo é menor ou igual a saque? {saldo <= saque}')
+print(f'saldo é igual a saque? {saldo == saque}')
+print(f'saldo é diferente de saque? {saldo != saque}')
+print(f'saldo é igual a saque? {saldo is saque}')
+print(f'saldo é diferente de saque? {saldo is not saque}')
