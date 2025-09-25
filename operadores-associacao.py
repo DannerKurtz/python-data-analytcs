828e8958f3a990b14072f5588ed29ad5a37b9e6c
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-25 20:56:50 -0300

    feat: add operadores-associacao

---
+++ operadores-associacao.py
@@ -0,0 +1,7 @@
+frutas = ['limão', 'laraja', 'limeira']
+
+print(f'limão está em frutas? {"limão" in frutas}')
+print(f'limão está em frutas? (negação) {"limão" not in frutas}')
+
+frase = 'teste de uma frase qualquer'
+print(f'qualquer está na frase {"qualquer" in frase}')
