6993809b289149a6d6c1a1147fcb1ad73916f5dc
Danner Kurtz <kurtz.danner@gmail.com>
2025-09-25 20:09:20 -0300

    feat: add operadores de identidade

---
+++ operadores-identidade.py
@@ -0,0 +1,11 @@
+saldo = 1000
+limite = 1000
+
+print(f'saldo e limete ocupam o mesmo espaço de memória? {saldo is limite}')
+print(f'saldo e limite ocupam o mesmo espaço de memória (negação)? {saldo is not limite}')
+
+nome = 'Jhon'
+sobrenome = 'Doe'
+
+print(f'nome e sobrenome ocupam o mesmo espaço de memória? {nome is sobrenome}')
+print(f'nome e sobrenome ocupam o mesmo espaço de memória (negação)? {nome is not sobrenome}')
